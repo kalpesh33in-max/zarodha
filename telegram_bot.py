d99db8b12374b49f6eaf07d7eadee89ce0f2ee0f
kalpesh33in-max <kalpesh33in@gmail.com>
2026-03-10 21:42:40 +0530

    Create telegram_bot.py

---
+++ telegram_bot.py
@@ -0,0 +1,14 @@
+import requests
+from config import TELEGRAM_TOKEN, CHAT_ID
+
+def send(msg):
+
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+
+    requests.post(
+        url,
+        data={
+            "chat_id": CHAT_ID,
+            "text": msg
+        }
+    )
