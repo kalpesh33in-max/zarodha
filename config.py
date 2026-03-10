faba07cc042543cb0201efef0070f71410d0c7dc
kalpesh33in-max <kalpesh33in@gmail.com>
2026-03-10 21:42:09 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+API_KEY = os.getenv("KITE_API_KEY")
+API_SECRET = os.getenv("KITE_API_SECRET")
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+LOGIN_TIME = "08:00"
